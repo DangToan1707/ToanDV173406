72d7668c0e187c5d33332b3c97ec4cbd35a6a06f
dangtoan <dangvantoan1707@gmail.com>
2021-01-06 17:27:52 +0700

    ...

---
+++ DangVanToan20173406_Lab05/Lab05-OMSApp/src/com/oms/factory/UserPageFactory.java
@@ -0,0 +1,24 @@
+package com.oms.factory;
+
+import com.oms.components.abstractdata.controller.ADataPageController;
+
+import javax.swing.*;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class UserPageFactory {
+    private static Map<String, JPanel> m_RegisteredPage = new HashMap<>();
+
+    private UserPageFactory() {
+
+    }
+
+    public synchronized static void register(String type, JPanel panel) {
+        m_RegisteredPage.put(type, panel);
+    }
+
+    public static JPanel createPane(String type) {
+        return m_RegisteredPage.get(type);
+    }
+}
